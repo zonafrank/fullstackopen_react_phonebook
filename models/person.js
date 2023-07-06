e70c6573b66508fba37ed812127b68c6eab8a683
Eche <echyth@yahoo.com>
2023-07-06 15:59:27 +1000

    ui biuld

--- models/person.js
+++ models/person.js
@@ -3,8 +3,21 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^\d{2}-\d{6,}$|^\d{3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`
+    },
+    required: [true, "phone number is required"]
+  }
 });
 
 personSchema.set("toJSON", {
